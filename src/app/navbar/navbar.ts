9025f0c2f8d55b59e12135bd6ca26eee36648b01
santhosh <san10thoshs@gmail.com>
2026-01-30 12:11:05 +0530

    navbar fixed

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [CommonModule,RouterLink],
+  imports: [CommonModule,RouterLink, RouterModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
 export class NavbarComponent {
   isMenuOpen = false;
+  isHidden = false;
+  private lastScrollTop = 0;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const currentScrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    if (currentScrollTop > this.lastScrollTop) {
+      // Scrolling down
+      this.isHidden = true;
+    } else {
+      // Scrolling up
+      this.isHidden = false;
+    }
+    this.lastScrollTop = currentScrollTop <= 0 ? 0 : currentScrollTop;
+  }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
